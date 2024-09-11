43bac353220458287e2c8f96b36057fd4ed5197d
Wawanahayy <98287702+Wawanahayy@users.noreply.github.com>
2024-09-11 21:49:06 +0700

    Update cat20.sh

--- cat20.sh
+++ cat20.sh
@@ -102,9 +102,9 @@ check_wallet_balance() {
 
 echo && echo -e " ${Red_font_prefix}dusk_network Skrip instalasi otomatis${Font_color_suffix} oleh \033[1;35Translete by TIMPLEXZ\033[0m
 Skrip ini sepenuhnya gratis dan open-source, dikembangkan oleh pengguna Twitter ATAU pemilik ${Green_font_prefix}@ouyoung11${Font_color_suffix}.
-echo "====================================================="
-echo "                 AIRDROP JAWA PRIDE                  "
-echo "====================================================="
+=====================================================
+==               AIRDROP JAWA PRIDE                == 
+=====================================================
 ———————————————————————
  ${Green_font_prefix} 1. Instalasi lingkungan dan node penuh ${Font_color_suffix}
  ${Green_font_prefix} 2. Buat dompet ${Font_color_suffix}
